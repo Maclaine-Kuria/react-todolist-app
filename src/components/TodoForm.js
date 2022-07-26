05e16bf45967ab160788143e5aa188b995e55a56
maclaine <maclainekuria@gmail.com>
2022-07-26 23:19:16 +0300

    todo setup

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,32 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 
 function TodoForm(props) {
-    const [input, setInput] = useState('');
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
 
-    const handleChange = e => {
-        setInput(e.target.value);
-    };
+  const handleSubmit = e => {
+    e.preventDefault();
 
-    const handleSumit = e => {
-        e.preventDefault();
-
-        props.onSubmit({
-            id:Math.floor(Math.random() * 10000),
-            text: input
-        });
-
-        setInput('');
-    };
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+    setInput('');
+  };
 
   return (
-    <form className='todo-form' onSubmit={handleSumit}>
-        <input type='text'
-         placeholder='Add a todo' 
-         value={input} 
-         onChange={handleChange}
-         name='text' 
-         className='todo-input'/>
-        <button className='todo-button'>Add todo</button>
+    <form onSubmit={handleSubmit} className='todo-form'>
+          <input
+            placeholder='Update your item'
+            type='text'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input' />
+            <button className='todo-button'>Add todo</button>
     </form>
   )
 }
